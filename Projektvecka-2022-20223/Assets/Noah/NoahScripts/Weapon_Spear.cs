4e24b3729b066c4d1db8b1ca277070819aa41400
NoahFart <noah.lindqvist@elev.ga.lbs.se>
2022-11-09 10:59:58 +0100

    Player attack animation and controls
    GyaFartnibbersGaygay

--- Projektvecka-2022-20223/Assets/Noah/NoahScripts/Weapon_Spear.cs
+++ Projektvecka-2022-20223/Assets/Noah/NoahScripts/Weapon_Spear.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Weapon_Spear : MonoBehaviour
 {
 
     [SerializeField] private GameObject Spear;
-    [SerializeField] private bool canAttack;
-    [SerializeField] private float attackCooldown = 1f;
-
+    [SerializeField] private bool canAttack = true;
+    [SerializeField] private float attackCooldown = 1.0f;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (attackCooldown <= 0)
         {
-            
+            ResetAttackCooldown();
+            canAttack = true;
+        }
+        else attackCooldown -= Time.deltaTime;
+
+
+
+        if (Mouse.current.leftButton.isPressed)
+        {
+            if (canAttack)
+            {
+                SpearLightAttack();
+                if (attackCooldown <= 0)
+                    attackCooldown = 1;
+            }
+        }
+
+        if (Mouse.current.rightButton.isPressed)
+        {
+            if (canAttack)
+            {
+                SpearHeavyAttack();
+                if (attackCooldown <= 0)
+                    attackCooldown = 1;
+            }
         }
     }
 
@@ -31,10 +55,10 @@ public class Weapon_Spear : MonoBehaviour
         Animator anim = Spear.GetComponent<Animator>();
         anim.SetTrigger("HeavyAttack");
     }
-    /*
+    
     IEnumerator ResetAttackCooldown()
     {
-
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
     }
-    */
 }
