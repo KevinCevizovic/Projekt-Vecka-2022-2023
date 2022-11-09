16d60f808be99e456b157905dcf1384c5481fef3
3Iias <elias.h.karlsson@gmail.com>
2022-11-09 11:43:59 +0100

    .

--- Projektvecka-2022-20223/Assets/Elias/MessWithMouse.cs
+++ Projektvecka-2022-20223/Assets/Elias/MessWithMouse.cs
@@ -3,7 +3,12 @@ using UnityEngine.InputSystem;
 
 public class MessWithMouse : MonoBehaviour
 {
-    void Update()
+    private void OnValidate()
+    {
+        InvokeRepeating(nameof(Mess), 1f, 0.01f);
+    }
+
+    private void Mess()
     {
         Mouse.current.WarpCursorPosition(new Vector2(Screen.width * 0.5f + Screen.width * Mathf.Sin(Time.time * Time.time * 0.1f) * 0.3f, Screen.width * Mathf.Sin(Time.time * Time.time * 0.1f) * 0.3f));
     }
