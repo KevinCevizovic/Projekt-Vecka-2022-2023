869a564f1cc3e7c683daef656eb9e6879df7fae9
3Iias <elias.h.karlsson@gmail.com>
2023-02-07 20:47:39 +0100

    Health simple custom editor
    exposing the current health variable

--- Projektvecka-2022-20223/Assets/Noah/NoahScripts/Health.cs
+++ Projektvecka-2022-20223/Assets/Noah/NoahScripts/Health.cs
@@ -1,15 +1,32 @@
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.InputSystem;
-
 
 public class Health : MonoBehaviour
 {
+#if UNITY_EDITOR
+    [CustomEditor(typeof(Health))]
+    public class HealthEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            Health script = (Health)target;
+
+            // health text
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Current Health", EditorStyles.label, GUILayout.MaxWidth(120));
+            EditorGUILayout.LabelField(script.CurrentHealth.ToString(), GUILayout.MaxWidth(200));
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+#endif
+
     [SerializeField] private float maxHealth = 100f;
+
     public float CurrentHealth { get; private set; }
 
-    public float damage;
-
-    private void Awake()
+    private void Start()
     {
         CurrentHealth = maxHealth;
     }
@@ -32,7 +49,7 @@ public class Health : MonoBehaviour
     {
         CurrentHealth += amount;
 
-        if(CurrentHealth > maxHealth)
+        if (CurrentHealth > maxHealth)
         {
             CurrentHealth = maxHealth;
         }
