d137a1dd883173908565df055eb2b04a64e56443
3Iias <elias.h.karlsson@gmail.com>
2023-02-09 15:41:31 +0100

    movement test script

--- Projektvecka-2022-20223/Assets/Elias/MovementTest.cs
+++ Projektvecka-2022-20223/Assets/Elias/MovementTest.cs
@@ -2,64 +2,64 @@ using UnityEngine;
 
 public class MovementTest : MonoBehaviour
 {
-    [SerializeField] bool rotateTowardMouse = true;
+    //[SerializeField] bool rotateTowardMouse = true;
 
-    [SerializeField] float walkingSpeed = 10, rotationSpeed = 5, maxRunningSpeed = 20;
-    private float speed, runningSpeed;
+    ////[SerializeField] float walkingSpeed = 10, rotationSpeed = 5, maxRunningSpeed = 20;
+    ////private float speed, runningSpeed;
 
-    [SerializeField] float lerpDuration = 1;
-    private float elapsedTime;
+    //[SerializeField] float lerpDuration = 1;
+    //private float elapsedTime;
 
-    [SerializeField] AnimationCurve curve;
+    //[SerializeField] AnimationCurve curve;
 
-    [SerializeField] Camera Camera;
-    [SerializeField] InputHandler _input;
+    //[SerializeField] Camera Camera;
+    //[SerializeField] InputHandler _input;
 
-    Vector3 lastPosition;
+    //Vector3 lastPosition;
 
-    private void Awake()
-    {
-        if (_input == null)
-            _input = GetComponent<InputHandler>();
+    //private void Awake()
+    //{
+    //    if (_input == null)
+    //        _input = GetComponent<InputHandler>();
 
-        if (Camera == null)
-            Camera = Camera.main;
-    }
+    //    if (Camera == null)
+    //        Camera = Camera.main;
+    //}
 
-    void Update()
-    {
-        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+    //void Update()
+    //{
+    //    var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
 
-        if (rotateTowardMouse)
-            RotateFromMouseVector();
-        else
-            RotateTowardMovementVector(lastPosition - transform.position);
+    //    if (rotateTowardMouse)
+    //        RotateFromMouseVector();
+    //    else
+    //        RotateTowardMovementVector(lastPosition - transform.position);
 
-        Move(targetVector);
+    //    Move(targetVector);
 
-        lastPosition = transform.position; // temporary
-    }
+    //    lastPosition = transform.position; // temporary
+    //}
 
-    private void Move(Vector3 targetVector)
-    {
-        transform.position += targetVector;
-    }
+    //private void Move(Vector3 targetVector)
+    //{
+    //    transform.position += targetVector;
+    //}
 
-    private void RotateFromMouseVector()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(_input.MousePosition);
+    //private void RotateFromMouseVector()
+    //{
+    //    Ray ray = Camera.main.ScreenPointToRay(_input.MousePosition);
 
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
-        {
-            var target = hitInfo.point;
-            target.y = transform.position.y;
-            transform.LookAt(target);
-        }
-    }
-    private void RotateTowardMovementVector(Vector3 movementDirection)
-    {
-        if (movementDirection.magnitude == 0) { return; }
-        var rotation = Quaternion.LookRotation(movementDirection);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed);
-    }
+    //    if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+    //    {
+    //        var target = hitInfo.point;
+    //        target.y = transform.position.y;
+    //        transform.LookAt(target);
+    //    }
+    //}
+    //private void RotateTowardMovementVector(Vector3 movementDirection)
+    //{
+    //    if (movementDirection.magnitude == 0) { return; }
+    //    var rotation = Quaternion.LookRotation(movementDirection);
+    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed);
+    //}
 }
