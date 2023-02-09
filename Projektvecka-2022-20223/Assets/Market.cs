be850cda6ab5b79926b0f260bf14ad58d20d43fd
KevinCevizovic <kevin.cevizovic@elev.ga.lbs.se>
2023-02-09 13:42:34 +0100

    Main menu and rock models

---
+++ Projektvecka-2022-20223/Assets/Market.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
