138d2ae8788632c4ac49f1edd3c16e59ae6d1191
3Iias <elias.h.karlsson@gmail.com>
2023-03-09 09:55:28 +0100

    Update PlaceOnGround.cs

--- Projektvecka-2022-20223/Assets/Scripts/PlaceOnGround.cs
+++ Projektvecka-2022-20223/Assets/Scripts/PlaceOnGround.cs
@@ -1,10 +1,33 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class PlaceOnGround : MonoBehaviour
 {
+#if UNITY_EDITOR
+    [CustomEditor(typeof(PlaceOnGround))]
+    public class PickupEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            PlaceOnGround script = (PlaceOnGround)target;
+            if (GUILayout.Button("PlaceOnGround"))
+            {
+                script._PlaceOnGround();
+            }
+            base.OnInspectorGUI();
+        }
+    }
+#endif
     [SerializeField] LayerMask ground = 1 << 9;
 
     private void Start()
+    {
+        _PlaceOnGround();
+    }
+
+    public void _PlaceOnGround()
     {
         if (Physics.Raycast((1000f * Vector3.up) + transform.position, Vector3.down, out var hit, 2000f, ground)) // check where ground is(1000m above or below is limit)
         {
