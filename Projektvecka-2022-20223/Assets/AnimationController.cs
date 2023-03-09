49cac13d1fa1271cc9d03ef962e0b2fc2f3f0efb
KevinCevizovic <kevin.cevizovic@elev.ga.lbs.se>
2023-03-09 14:28:42 +0100

    Animation controller player working

--- Projektvecka-2022-20223/Assets/AnimationController.cs
+++ Projektvecka-2022-20223/Assets/AnimationController.cs
@@ -5,6 +5,8 @@ public class AnimationController : MonoBehaviour
     Animator anim;
     TopDownCharacterMover movement;
 
+    Vector3 moveDirection;
+
     void Awake()
     {
         movement = GetComponent<TopDownCharacterMover>();
@@ -16,9 +18,19 @@ public class AnimationController : MonoBehaviour
     {
         //if(transform.forward)
         //    anim.SetFloat
+        //var e= Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0) * movement.MoveDir;
+        //var e = movement.MoveDir + transform.forward;
+        //var e = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * movement.MoveDir;
 
-        
-        anim.SetFloat("x", movement.InputVector.x);
-        anim.SetFloat("y", movement.InputVector.y);
+        //moveDirection = moveDirection.normalized;
+
+        moveDirection = transform.InverseTransformDirection(movement.MoveDir);
+        //Debug.Log(moveDirection);
+        //moveDirection = Vector3.zero;
+        //Debug.Log("ZERO");
+
+        Debug.Log(moveDirection);
+        anim.SetFloat("x", moveDirection.x, .3f, Time.deltaTime);
+        anim.SetFloat("y", moveDirection.z, .3f, Time.deltaTime);
     }
 }
