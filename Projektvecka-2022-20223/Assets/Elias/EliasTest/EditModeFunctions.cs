b907a5fd9856d44685884cfd823e568bd14cf19d
KevinCevizovic <kevin.cevizovic@elev.ga.lbs.se>
2023-03-10 11:58:24 +0100

    final

--- Projektvecka-2022-20223/Assets/Elias/EliasTest/EditModeFunctions.cs
+++ Projektvecka-2022-20223/Assets/Elias/EliasTest/EditModeFunctions.cs
@@ -1,31 +1,31 @@
-using UnityEngine;
-using UnityEditor;
+//using UnityEngine;
+//using UnityEditor;
 
-public class EditModeFunctions : EditorWindow
-{
-    [MenuItem("Window/Edit Mode Functions")]
-    public static void ShowWindow()
-    {
-        GetWindow<EditModeFunctions>("Edit Mode Functions");
-    }
+//public class EditModeFunctions : EditorWindow
+//{
+//    [MenuItem("Window/Edit Mode Functions")]
+//    public static void ShowWindow()
+//    {
+//        GetWindow<EditModeFunctions>("Edit Mode Functions");
+//    }
 
-    private void OnGUI()
-    {
-        if (GUILayout.Button("Spawn all items"))
-        {
-            ItemShower[] itemShowerArray = FindObjectsOfType<ItemShower>();
-            foreach (ItemShower itemShower in itemShowerArray)
-                if (itemShower.Item != null && itemShower.Item._object != null)
-                    Instantiate(itemShower.Item._object, itemShower.transform.position, itemShower.transform.rotation, itemShower.transform);
-        }
+//    private void OnGUI()
+//    {
+//        if (GUILayout.Button("Spawn all items"))
+//        {
+//            ItemShower[] itemShowerArray = FindObjectsOfType<ItemShower>();
+//            foreach (ItemShower itemShower in itemShowerArray)
+//                if (itemShower.Item != null && itemShower.Item._object != null)
+//                    Instantiate(itemShower.Item._object, itemShower.transform.position, itemShower.transform.rotation, itemShower.transform);
+//        }
 
-        if (GUILayout.Button("Delete all items"))
-        {
-            ItemShower[] itemShowerArray = FindObjectsOfType<ItemShower>();
-            foreach (ItemShower itemShower in itemShowerArray)
-                if (itemShower.Item != null && itemShower.Item._object != null)
-                    while (itemShower.transform.childCount > 0)
-                        DestroyImmediate(itemShower.transform.GetChild(itemShower.transform.childCount - 1).gameObject, false);
-        }
-    }
-}
+//        if (GUILayout.Button("Delete all items"))
+//        {
+//            ItemShower[] itemShowerArray = FindObjectsOfType<ItemShower>();
+//            foreach (ItemShower itemShower in itemShowerArray)
+//                if (itemShower.Item != null && itemShower.Item._object != null)
+//                    while (itemShower.transform.childCount > 0)
+//                        DestroyImmediate(itemShower.transform.GetChild(itemShower.transform.childCount - 1).gameObject, false);
+//        }
+//    }
+//}
